56f1983400cbedcb33688edbd373580bd8f68586
Brunhilde_J_Laptop <gksjoe@gmail.com>
2023-05-30 22:06:05 -0700

    Created AlarmMaker and AlarmManager classes. Adjusted formatting on MainPage.

---
+++ QR Alarm/AlarmMaker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR_Alarm
+{
+    public class AlarmMaker
+    {
+        private DateTime alarmTime;
+        private bool isOn;
+        private bool isSnooze;
+        private int volume;
+        private int alarmTone;
+        private int alarmType;
+
+        public AlarmMaker(DateTime alarmTime, bool isOn=true, bool isSnooze=false, int volume=100, int alarmTone=0, int alarmType=0)
+        {
+            this.alarmTime = alarmTime;
+        }
+    }
+}
---
+++ QR Alarm/AlarmManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR_Alarm
+{
+    public class AlarmManager
+    {
+        private List<AlarmMaker> AlarmList = new List<AlarmMaker>(100);
+
+
+    }
+
+    public void AlarmEdit(int selectedAlarm)
+    {
+        get 
+    }
+}
